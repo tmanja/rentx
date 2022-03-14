3febec58444db922ef1a92de4f58b5bf90c35a9d
Thalles <thalles.devraiz@gmail.com>
2022-03-14 11:52:14 -0300

    create Specification model

---
+++ src/model/Specification.ts
@@ -0,0 +1,15 @@
+import { v4 as uuidV4 } from "uuid";
+
+export class Specification {
+  id: string;
+  name: string;
+  description: string;
+  created_at: Date;
+
+  constructor(name: string, description: string) {
+    this.id = uuidV4();
+    this.name = name;
+    this.description = description;
+    this.created_at = new Date();
+  }
+}
