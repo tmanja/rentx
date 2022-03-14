0ee7113c91a4cf68ba7d5a8ebb3feb494051549a
Thalles <thalles.devraiz@gmail.com>
2022-03-14 14:01:21 -0300

    create category usecase

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,16 +1,13 @@
 import { Router } from "express";
 
 import { CategoriesRepository } from "../cars/repositories/CategoriesRepository";
-import { CreateCategoryService } from "../cars/services/CreateCategoryService";
+import { createCategoryController } from "../cars/useCases/createCategory";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (req, res) => {
-  const { name, description } = req.body;
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-  createCategoryService.execute({ name, description });
-  return res.status(201).end();
+  return createCategoryController.handle(req, res);
 });
 
 categoriesRoutes.get("/", (_req, res) => {
