d9d71745821b893249a166b6a468ea689aed6139
Thalles <thalles.devraiz@gmail.com>
2022-03-13 21:38:34 -0300

    add express body parser json middleware

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
+
+app.use(express.json());
 
 app.listen(3333, () => {
   console.log("Server is running");
